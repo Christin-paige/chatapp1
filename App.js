31f6d936e7b8a863d1774e8c78cce71820570df0
Christin-paige <christinpmartin@gmail.com>
2023-12-16 19:36:23 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+// import the screens
+import Screen1 from './components/Screen1';
+import Screen2 from './components/Screen2';
+
+//import react navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
+
+
+const App = () => {
+ return (
+   <NavigationContainer>
+     <Stack.Navigator
+     initialRouteName="Screen1"
+     >
+       <Stack.Screen
+         name="Screen1"
+         component={Screen1}
+         />
+       <Stack.Screen
+       name="Screen2"
+       component={Screen2}
+       /> 
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
+}
+
+
+
+
+export default App;
+
